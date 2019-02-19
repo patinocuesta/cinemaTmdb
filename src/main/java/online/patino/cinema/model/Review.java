4543d2803950c7082bd16f94267898c7434a2f44
donato <patinodonato@gmail.com>
2019-02-19 16:01:00 +0100

    TMDB

--- src/main/java/online/patino/cinema/model/Review.java
+++ src/main/java/online/patino/cinema/model/Review.java
@@ -1,10 +1,39 @@
 package online.patino.cinema.model;
 
+import org.springframework.transaction.IllegalTransactionStateException;
+
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 public class Review {
+
+
+
+    private static final int WAITING_MODERATION = 1;
+    private static final int PUBLISHED = 2;
+    private static final int MUST_BE_MODIFIED = 3;
+    private static final int REJECTED = 4;
+    private static final int DELETED = 5;
+    private static final int ABANDONED = 6;
+
+    private static  final   Map<Integer, Integer[]> transitions;
+      static {
+          transitions = new HashMap<>();
+          transitions.put(Review.REJECTED, new Integer[]{Review.WAITING_MODERATION});
+          transitions.put(Review.DELETED, new Integer[]{Review.PUBLISHED});
+          transitions.put(Review.PUBLISHED, new Integer[]{Review.WAITING_MODERATION});
+          transitions.put(Review.MUST_BE_MODIFIED, new Integer[]{Review.WAITING_MODERATION});
+          transitions.put(Review.ABANDONED, new Integer[]{Review.MUST_BE_MODIFIED});
+          transitions.put(Review.WAITING_MODERATION, new Integer[]{Review.WAITING_MODERATION, Review.PUBLISHED, Review.MUST_BE_MODIFIED});
+      }
+
+    private int state = Review.WAITING_MODERATION;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -16,10 +45,10 @@ public class Review {
     @Column(name = "datte", nullable = false)
     private Timestamp date;
     @ManyToOne
-    @JoinColumn(name="film_id")
+    @JoinColumn(name = "film_id")
     private Film film;
     @ManyToOne
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     public long getId() {
@@ -84,4 +113,40 @@ public class Review {
         result = 31 * result + (date != null ? date.hashCode() : 0);
         return result;
     }
+
+    public static int getATTENTE_MODERATION() {
+        return WAITING_MODERATION;
+    }
+
+    public static int getPUBLIE() {
+        return PUBLISHED;
+    }
+
+    public static int getATTENTE_MODIFICATION() {
+        return MUST_BE_MODIFIED;
+    }
+
+    public static int getREJETE() {
+        return REJECTED;
+    }
+
+    public static int getSUPPRIME() {
+        return DELETED;
+    }
+
+    public static int getABANDONNE() {
+        return ABANDONED;
+    }
+
+    public int getState() {
+        return this.state;
+    }
+
+    private boolean  canTransitTo(int targetState) {
+
+        return Arrays.asList(transitions.get(targetState)).contains(this.getState());
+    }
+
+
 }
+
---
+++ src/test/java/online/patino/cinema/model/ReviewTest.java
@@ -0,0 +1,145 @@
+package online.patino.cinema.model;
+
+import org.junit.Test;
+import org.springframework.transaction.IllegalTransactionStateException;
+
+import static org.junit.Assert.*;
+
+public class ReviewTest {
+
+    @Test
+    public void setArticle() {
+        Review comment = new Review();
+        comment.setArticle("test commentaire");
+        assertEquals("test commentaire", comment.getArticle());
+    }
+
+    @Test
+    public void etatInitial() {
+        Review comment = new Review();
+        assertEquals(Review.WAITING_MODERATION, comment.getState());
+    }
+
+    @Test
+    public void goodTransitionToPublie() {
+        Review comment = new Review();
+        try {
+            comment.validByModerator();
+            assertEquals(Review.PUBLISHED, comment.getState());
+        } catch (IllegalTransitionStateException e) {
+            fail("Transition attendue");
+        }
+    }
+
+    @Test
+    public void badTransitionToPublie() {
+        Review comment = new Review();
+        try {
+            comment.keepForEditByModerator();
+            comment.validByModerator();
+            fail("Transition non autorisée");
+        } catch (IllegalTransitionStateException e) {
+            assertEquals(Review.MUST_BE_MODIFIED, comment.getState());
+        }
+    }
+
+    @Test
+    public void goodTransitionToAbandonne() {
+        Review comment = new Review();
+        try {
+            comment.keepForEditByModerator();
+            comment.abandonByUser();
+            assertEquals(Review.ABANDONED, comment.getState());
+        } catch (IllegalTransitionStateException e) {
+            fail("Transition attendue");
+        }
+    }
+
+    @Test
+    public void badTransitionToAbandoned() {
+        Review comment = new Review();
+        try {
+            comment.validByModerator();
+            comment.abandonByUser();
+            fail("Transition vers Abandonne non autorisée");
+        } catch (IllegalTransitionStateException e) {
+            assertEquals(Review.PUBLISHED, comment.getState());
+        }
+    }
+
+    @Test
+    public void goodTransitionReject() {
+        Review comment = new Review();
+        try {
+            comment.rejectByModerator();
+            assertEquals(Review.REJECTED, comment.getState());
+        } catch (IllegalTransitionStateException e) {
+            fail("Transition attendue");
+        }
+    }
+
+    @Test
+    public void badTransitionReject() {
+        Review comment = new Review();
+        try {
+            comment.validByModerator();
+            comment.rejectByModerator();
+            fail("Transition vers Rejected non autorisée");
+        } catch (IllegalTransitionStateException e) {
+            assertEquals(Review.PUBLISHED, comment.getState());
+        }
+    }
+
+
+    @Test
+    public void goodTransitionEditByUserFromPublished() {
+        Review comment = new Review();
+        try {
+            comment.validByModerator();
+            comment.editByUser();
+            assertEquals(Review.WAITING_MODERATION, comment.getState());
+        } catch (IllegalTransitionStateException e) {
+            fail("Transition attendue");
+        }
+    }
+
+
+    @Test
+    public void goodTransitionEditByUserFromWAITING_MODERATION() {
+        Review comment = new Review();
+        try {
+            assertEquals(Review.WAITING_MODERATION, comment.getState());
+            comment.editByUser();
+            assertEquals(Review.WAITING_MODERATION, comment.getState());
+        } catch (IllegalTransitionStateException e) {
+            fail("Aucune transition attendue");
+        }
+    }
+
+
+    @Test
+    public void goodTransitionEditByUserFromMUST_BE_MODIFIED() {
+        Review comment = new Review();
+        try {
+            comment.keepForEditByModerator();
+            assertEquals(Review.MUST_BE_MODIFIED, comment.getState());
+            comment.editByUser();
+            assertEquals(Review.WAITING_MODERATION, comment.getState());
+        } catch (IllegalTransitionStateException e) {
+            fail("Transition attendue");
+        }
+    }
+
+    @Test
+    public void badTransitionEditByUserFromABANDONED() {
+        Review comment = new Review();
+        try {
+            comment.keepForEditByModerator();
+            comment.abandonByUser();
+            comment.editByUser();
+            fail("Transition non permise attendue");
+        } catch (IllegalTransitionStateException e) {
+            assertEquals(Review.ABANDONED, comment.getState());
+        }
+    }
+}
