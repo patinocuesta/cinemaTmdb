e0a42c66b9b5c893630c3efde80b7d85ad194a97
donato <patinodonato@gmail.com>
2019-03-01 16:55:31 +0100

    TMDB pagination onprgress

---
+++ src/main/java/online/patino/cinema/web/IndexController.java
@@ -0,0 +1,18 @@
+package online.patino.cinema.web;
+
+import online.patino.cinema.service.TmdbRestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+    @Autowired
+   // private TmdbRestClient tmdbRestClient;
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
